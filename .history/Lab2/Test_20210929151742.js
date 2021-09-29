5299ae87cb3b4eec052c256a8e27491ee4f920a6
ChristeZeng <christezeng@gmail.com>
2021-09-29 15:18:12 +0800

    test alb2

---
+++ .history/Lab2/Test_20210929151742.js
@@ -0,0 +1,16 @@
+function main()
+{
+    const canvas = document.querySelector("#glcanvas");
+    const gl = canvas.getContext("webgl");
+    if(!gl)
+    {
+        alert("No WebGL");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+
+`;
